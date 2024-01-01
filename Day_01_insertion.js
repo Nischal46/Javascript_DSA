43800f07745f772c464f78dba7967f091ae8df17
Nischal <baniyanisal12@gmail.com>
2024-01-01 23:02:46 +0545

    Implement of JS built in method splice to insert element in the array

--- Day_01_insertion.js
+++ Day_01_insertion.js
@@ -2,10 +2,14 @@
 const readlinesync = require('readline-sync');
 
 let arraydata = [60, 70, 80, 90];
+
+console.log('Array is ', arraydata);
+
 const newelement = +(readlinesync.question('Enter a number: '));
 
-const position = +(readlinesync.questionInt('Enter a position to insert in the array: '));
-
+const position = +(readlinesync.question('Enter a position to insert in the array: '));
+ 
+// Logic of inserting the element inside loop of array
 for(let i = arraydata.length-1; i>=0; i--){
     console.log(arraydata[i]);
 
@@ -18,4 +22,16 @@ for(let i = arraydata.length-1; i>=0; i--){
 }
 
 console.log(arraydata);
+console.log('--------------------------------------');
+
+//javascript built in method 
+//splice method performs actually as the same logic like the above
+
+let newarrayData = [34, 54, 23, 56];
+
+newarrayData.splice(2,0,47); // In this splice the first is index or start of the element, second is to delete and the third is the item that is to be added. 
+
+console.log(newarrayData);
+
+
 
