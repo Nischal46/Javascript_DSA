6095092e2aac879c98f21dd28eb5857021129152
Nischal <baniyanisal12@gmail.com>
2024-01-05 23:52:34 +0545

    made a simple program that performs the operation of insertion deletion and searching of the element that is combine from day 1 2 3 and 4

---
+++ Day_05_Jan_2024.js
@@ -0,0 +1,84 @@
+//Day 5 Programming
+const array = [32, 54,65,34,22];
+console.log('The initial array, ', array);
+console.log("-------------------------Program--------------------");
+
+console.log(`
+Press
+1. Insertion of the element of the array
+2. Deletion of the element of the array
+3. Searching of the index of the array
+4. Merging of multiple array
+`);  
+
+const readlinesync = require('readline-sync'); 
+const userinput = +readlinesync.question('Enter a choice for the operation: ');
+
+
+switch(userinput){
+    case 1:
+        console.log('You had choosen for the operation of adding element in the array: ');
+        const addedElement = +readlinesync.question('Enter the element: ');
+
+
+        const index = +readlinesync.question('enter a position to insert the element: ');
+
+
+        for(let i = array.length - 1; i>=0; i--){
+            if(i>=index){
+                array[i] = array[i+1];
+
+                if(i === index){
+                    array[i] = addedElement;
+                }
+            }
+        }
+
+        console.log('The array after the insertion of the element, ', array);
+
+        break;
+
+
+    case 2:
+        console.log('You had choosen for the operation of the deletion element in the array: ');
+        const deleteelementIndex = +readlinesync.question("Enter the index of the elemen that need to be delete: ");
+
+        for(let i = deleteelementIndex; i<=array.length - 1; i++){
+            array[i] = array[i + 1];
+        }
+
+        array.length = array.length - 1;
+        console.log(array);
+
+        break;     
+
+
+    case 3:
+        console.log("You have choosen for the operation of the searching element index in the array");
+        let findIndex;
+        const searchelement = +readlinesync.question("Enter a element that need to be search of its index: ");
+
+        for(let i=0; i<=array.length - 1; i++){
+            if(array[i] === searchelement){
+                findIndex = i
+                break;
+            }
+        }
+        console.log(`The element that you have entered is ${searchelement} and the element found at the index of ${findIndex}`);
+
+        break;
+
+
+    // case 4:
+    //     console.log("You have choosen for the operation of the merging of the multiple array");
+
+    //     const arrayOne = [];
+
+    //     const arrayTwo = [];
+
+    //     for(let i = 0; i<= 4; i++ ){
+    //         const inputarray1 = +readlinesync.question("Enter")
+    //         arrayOne[i] = 
+    //     }
+
+}
