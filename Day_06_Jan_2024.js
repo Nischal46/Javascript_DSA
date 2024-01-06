c47c03e430382c4d9256178dff82a740c6b96fc7
Nischal <baniyanisal12@gmail.com>
2024-01-06 23:12:39 +0545

    Sorting of the array data in both ascending and descending order using switch case for user choice

--- Day_06_Jan_2024.js
+++ Day_06_Jan_2024.js
@@ -1,17 +1,54 @@
 // Sorting of the array
+const readlinesync = require('readline-sync');
 
 let array = [34,12,54,76,9];
 
-console.log('------------------Program');
+console.log('The initial array data is ', array);
 
-for(let i = 0; i<array.length; i++){
-    for(let j= 0; j<array.length; j++){
-        if(array[j] > array[j+1]){
-            let temporaryValue = array[j];
-        array[j] = array[j+1];
-        array[j+1] = temporaryValue;
+console.log(`
+--> Press 1 to arrange the array data in ascending order
+--> Press 2 to arrange the array data in descending order
+`)
+
+const userChoice = +readlinesync.question('Enter what you want to perform: ')
+
+console.log('------------------Program--------------------');
+
+switch(userChoice){
+    case 1:
+        for(let i = 0; i<array.length; i++){
+            for(let j= 0; j<array.length; j++){
+                if(array[j] > array[j+1]){
+                    let temporaryValue = array[j];
+                array[j] = array[j+1];
+                array[j+1] = temporaryValue;
+                }
+            }
         }
-    }
+        console.log('Arranging array data in ascending order: ', array);
+
+        break;
+
+        case 2:
+            for(let i = 0; i<array.length; i++){
+                for(let j = 0; j<array.length; j++){
+                    if(array[j] < array[j+1]){
+                        let temp = array[j];
+                        array[j] = array[j+1];
+                        array[j+1] = temp;
+                    }
+                }
+            }
+
+            console.log('Arranging array data in descending order: ', array);
+
+            break;
+
+        default:
+            console.log('Invalid input from the user. Please choose a valid input for the operation');
+
+            break;
+
 }
 
-console.log(array);
+
