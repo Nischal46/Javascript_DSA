042126e14991e05a886a1bbe1b72fe27553e9325
Nischal <baniyanisal12@gmail.com>
2024-01-08 23:13:37 +0545

    Implement winning logic

--- Day_08_Jan_2024.js
+++ Day_08_Jan_2024.js
@@ -23,99 +23,105 @@ log(`${colors.green}Scissor${colors.reset}-${colors.blue}Paper${colors.reset}-${
 
 for (let i = 0; i<3; i++){
 
-log('****************************************************')
-log(`                                             count: ${i+1} | ${colors.red}Player 1 win : ${player1win}${colors.reset} | ${colors.green}Player 2 win: ${player2win}${colors.reset}`)
+    function Winner(player){
+        if(player === 'cpu')  player1win++;
+        else if(player === 'player')  player2win++;
+        log(`${colors.green}${player} wins ! ${colors.reset}`);
+    }
+    
+    function Draw(){
+        log(`${colors.red}It's a Draw !${colors.reset}`)
+    }
+    
+
+    log('****************************************************')
+    log(`                                             count: ${i+1} | ${colors.red}CPU win : ${player1win}${colors.reset} | ${colors.green}Player win: ${player2win}${colors.reset}`)
 // const userinput = +input.question("Press Enter to start: ")
 
-function GenerateOutcomes(player){
-    let outcomesEmoji = possibleOutcomes[Math.floor(Math.random() * possibleOutcomes.length)];
-    let result;
+    function GenerateOutcomes(player){
+        let outcomesEmoji = possibleOutcomes[Math.floor(Math.random() * possibleOutcomes.length)];
+        let result;
 
-    if(outcomesEmoji === 'scissor') result = `${outcomesEmoji} 🤞`;
+        if(outcomesEmoji === 'scissor') result = `${outcomesEmoji} 🤞`;
 
-    else if(outcomesEmoji === 'rock') result = `${outcomesEmoji} 👊`;
+        else if(outcomesEmoji === 'rock') result = `${outcomesEmoji} 👊`;
 
-    else if(outcomesEmoji === 'paper') result = `${outcomesEmoji} ✋`;
+        else if(outcomesEmoji === 'paper') result = `${outcomesEmoji} ✋`;
 
-    return result;
+        return result;
 
-}
-
-function Winner(player){
-    if(player === 'player1') player1win+=1;
-    else if(player === 'player2') player2win+=1;
-    log(`${player} win`);
-}
-
-function checkWinningCondition() {
-
-    
-    // let player2 = GenerateOutcomes('player 2'); 
-    log(`
-    --> Press 1 for rock 👊
-    --> Press 2 for scissor 🤞
-    --> Press 3 for paper ✋
-    `)
-    const userchoice = +input.question(`${colors.blue}Enter your choice: ${colors.reset}`)
-    log(`
-    
-    `)
-
-    let player;
-
-    switch(userchoice){
-        case 1:
-            player = "rock 👊";
-            break;
-
-        case 2:
-            player = "scissor 🤞";
-            break;
-
-        case 3:
-            player = 'paper ✋';
-            break;
-
-        default:
-            log('Invalid input');
-            break;
     }
 
-    let cpu = GenerateOutcomes('cpu');
-    log('Player 1: ', cpu);
 
-    log('Player 2: ', player);
+    function checkWinningCondition() {
+
+        
+        // let player2 = GenerateOutcomes('player 2'); 
+        log(`
+        --> Press 1 for rock 👊
+        --> Press 2 for scissor 🤞
+        --> Press 3 for paper ✋
+        `)
+        const userchoice = +input.question(`${colors.blue}Enter your choice: ${colors.reset}`)
+        log(`
+        
+        `)
+
+        let player;
+
+        switch(userchoice){
+            case 1:
+                player = "rock 👊";
+                break;
+
+            case 2:
+                player = "scissor 🤞";
+                break;
+
+            case 3:
+                player = 'paper ✋';
+                break;
+
+            default:
+                log('Invalid input');
+                break;
+        }
+
+        let cpu = GenerateOutcomes('cpu');
+        log('CPU: ', cpu);
+
+        log('Player: ', player);
+
+        log(`
+        
+        `)
+
+        if(cpu.includes('rock') && player.includes('scissor')) return Winner('cpu');
+
+        else if(cpu.includes('scissor') && player.includes('paper')) return Winner('cpu');
+
+        else if(cpu.includes('paper') && player.includes('rock')) return Winner('cpu');
+
+        else if(cpu.includes('scissor') && player.includes('rock')) return Winner('player');
+
+        else if(cpu.includes('paper') && player.includes('scissor')) return Winner('player');
+
+        else if(cpu.includes('rock') && player.includes('paper')) return Winner('player');
+
+        else if(cpu.includes('rock') && player.includes('rock')) return Draw();
+
+        else if(cpu.includes('scissor') && player.includes('scissor')) return Draw();
+
+        else if(cpu.includes('paper') && player.includes('paper')) return Draw();
+
+    }
+
+
+    checkWinningCondition();
 
     log(`
-    
+
     `)
-
-    if(cpu.includes('rock') && player.includes('scissor')) return Winner('cpu');
-
-    else if(cpu.includes('scissor') && player.includes('paper')) return Winner('cpu');
-
-    else if(cpu.includes('paper') && player.includes('rock')) return Winner('cpu');
-
-    else if(cpu.includes('scissor') && player.includes('rock')) return Winner('player');
-
-    else if(cpu.includes('paper') && player.includes('scissor')) return Winner('player');
-
-    else if(cpu.includes('rock') && player.includes('paper')) return Winner('player');
-
-    else if(cpu.includes('rock') && player.includes('rock')) return log('Draw');
-
-    else if(cpu.includes('scissor') && player.includes('scissor')) return log('Draw');
-
-    else if(cpu.includes('paper') && player.includes('paper')) return log('Draw');
-
-}
-
-
-checkWinningCondition();
-
-log(`
-
-`)
 
 }
 
