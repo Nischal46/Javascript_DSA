8b8d90a3fe3256878c056292f81806796ebce143
Nischal <baniyanisal12@gmail.com>
2024-01-12 20:22:06 +0545

    building tictactoe board

---
+++ eXTRA/tic-tac-toe.js
@@ -0,0 +1,23 @@
+const {log} = require('console');
+
+log("Tic-Tac-Toe");
+
+let board = [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
+]
+
+function DisplayBoard(){
+    log(`
+    ${board[0][0]} | ${board[0][1]} | ${board[0][2]}
+    -------
+    ${board[1][0]} | ${board[1][1]} | ${board[1][2]}
+    -------
+    ${board[2][0]} | ${board[2][1]} | ${board[2][2]}
+
+    `)
+}
+
+board[0][0] = 'X';
+DisplayBoard();
