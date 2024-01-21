684c66ce3aa83d3744703983dcfaf3cc9f9abd50
Nischal <baniyanisal12@gmail.com>
2024-01-21 20:20:35 +0545

    deleting of the data using splice method in js

--- 01_January/Day_17_Jan_2024.js
+++ 01_January/Day_17_Jan_2024.js
@@ -7,3 +7,8 @@ let details = ['engineer', 'doctor', 'pilot', 'lawyer'];
 details.sort((a,b) => a.localeCompare(b));
 
 console.log(details);
+
+const details1 = [12,43,54,656,777];
+
+details1.splice(0, 1);
+console.log(details1);
