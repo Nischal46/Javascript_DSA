f179cc295bd401c98ea7d0cd5b572d9d3e6c0595
Nischal <baniyanisal12@gmail.com>
2024-01-21 20:13:15 +0545

    sorting the array data using sort method

--- 01_January/Day_17_Jan_2024.js
+++ 01_January/Day_17_Jan_2024.js
@@ -1,3 +1,9 @@
 const studentArray =['hari', 'om', 'jagdish'];
 studentArray.splice(0,0,'ram');
 console.log(studentArray);;
+//for the deletion of the data ion array
+
+let details = ['engineer', 'doctor', 'pilot', 'lawyer'];
+details.sort((a,b) => a.localeCompare(b));
+
+console.log(details);
