7e4e595481838bd31d541b1af0c8b4daef598054
Nischal <baniyanisal12@gmail.com>
2024-01-22 23:05:56 +0545

    practicing every and some array built in method in js

---
+++ 01_January/Day_18_Jan_2024.js
@@ -0,0 +1,9 @@
+//practice builtin array method in js
+
+const array1 = ['php', 'node', 'java', 'javascript', 'python'];
+
+const array2 = ['php', 'node', 'java', 'javascript', 'python'];
+
+const checkarray = array1.every(cl => array2.some(nl => cl.includes(nl)));
+
+console.log(checkarray);
