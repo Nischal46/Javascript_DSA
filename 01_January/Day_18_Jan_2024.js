191d2ffc24dcdacbe151f051b2ae003205600250
Nischal <baniyanisal12@gmail.com>
2024-01-22 23:10:27 +0545

    Practicing indexof js method

--- 01_January/Day_18_Jan_2024.js
+++ 01_January/Day_18_Jan_2024.js
@@ -7,3 +7,10 @@ const array2 = ['php', 'node', 'java', 'javascript', 'python'];
 const checkarray = array1.every(cl => array2.some(nl => cl.includes(nl)));
 
 console.log(checkarray);
+
+
+
+
+// indexof
+const name = ['ramesh', 'ravi', 'lal bahadur'];
+console.log(name.indexOf('ravi'))
