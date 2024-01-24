5a47d8be0a937bfd374935b1ac266dc439109157
Nischal <baniyanisal12@gmail.com>
2024-01-24 22:16:19 +0545

    adding sorting feature in the program

--- 01_January/Day_19_Jan_2024.js
+++ 01_January/Day_19_Jan_2024.js
@@ -34,7 +34,7 @@ while(gameStart){
             const noOfturn = +input.question('Enter a no of the student details to add: ');
 
             function addstudentDetails(){
-                const id = Math.floor(Math.random() * 4)
+                const id = Math.floor(Math.random() * 100)
                 const name = input.question("Enter student Name: ");
                 const age = +input.question("Enter student Age: ");
                 const email = input.question("Enter student email address: ");
@@ -81,11 +81,51 @@ while(gameStart){
                     studentDetialsSearch[i] = studentArray[i];
                 }
             }
-            log('The search of the details of the student ', studentArray)
+            log('The search of the details of the student ', studentDetialsSearch)
             break;
 
         case 5:
-            log('This is the witch case of the sorting of the array');
+            log('Sorting of the element');
+            log(`
+            --> Press 1 to sort data in ascending order
+            --> Press 2 to sort data in descending order
+            `)
+            const sortChoice = +input.question('Enter a choice to sort data: ');
+            switch(sortChoice){
+                case 1:
+                    log('Sorting data in ascending order');
+                    for(let i = 0; i < studentArray.length; i++){
+                        for(let j = 0; j < studentArray.length; j++){
+                            if(studentArray[j].name > studentArray[j + 1].name){
+                                let temp = studentArray[j];
+                                studentArray[j] = studentArray[j + 1];
+                                studentArray[j + 1] = temp;
+                            }
+                        }
+                    }
+
+                    log('Sorting result ', studentArray);
+                    break;
+
+                case 2:
+                    log('Sorting data in descending order');
+                    for(let i = 0; i < studentArray.length; i++){
+                        for(let j= 0; j < studentArray.length; j++){
+                            if(studentArray[j].name < studentArray[j + 1].name){
+                                let temp = studentArray[j];
+                                studentArray[j] = studentArray[j + 1];
+                                studentArray[j + 1] = temp;
+                            }
+                        }
+
+                        log('Sorting result: ', studentArray);
+                        break;
+                    }
+
+                default:
+                    log('Invalid input. Please choose 1 or 2 to sort data');
+                    break;
+            }
             break;
 
         case 6:
