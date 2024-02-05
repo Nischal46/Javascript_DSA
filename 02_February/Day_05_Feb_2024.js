1a122d638b8140933a2ad59bbffda27009121d08
Nischal <baniyanisal12@gmail.com>
2024-02-05 22:30:27 +0545

    making simple program in factory function and constructor function

--- 02_February/Day_05_Feb_2024.js
+++ 02_February/Day_05_Feb_2024.js
@@ -16,3 +16,34 @@ let anotherarray = {
 console.log('object of books', anotherarray);
 const objectmergeintoarray = Object.values(anotherarray).reduce((acc, arr) => acc.concat(arr), []);
 console.log('final array from object', objectmergeintoarray);
+
+//constructor function
+
+function Person(name, email){
+    this.name = name;
+    this.email = email;
+
+    this.greeting = function(){
+        console.log(`Hey ${this.name} Congratulations. Your email address is ${this.email}`);
+    
+    }
+}
+
+const person1 = new Person('Nischal', 'nisal12@gmail.com');
+person1.greeting();
+
+//factory function
+
+function factoryFunction(name, stack){
+    return {
+        name,
+        stack,
+
+        description(){
+            console.log(`Hello I am ${name} and I am ${stack} developer`)
+        }
+    }
+}
+
+const dev = factoryFunction('Nischal', 'fullstack');
+dev.description();
