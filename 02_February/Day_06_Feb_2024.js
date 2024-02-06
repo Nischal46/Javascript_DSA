e789711bcd9ea997e7b164ce9c1e09304b6b0e11
Nischal <baniyanisal12@gmail.com>
2024-02-06 22:21:32 +0545

    implement deleting data in queue

--- 02_February/Day_06_Feb_2024.js
+++ 02_February/Day_06_Feb_2024.js
@@ -19,9 +19,27 @@ function displayqueue(){
     console.log('the current queue is ', queue);
 }
 
+function dequeue(){
+    if(queuesize > 0){
+        for(let i=0; i < queue.length; i++){
+            queue[i] = queue[i+1];
+        }
+        queuesize--;
+        queue.length = queuesize;
+    }
+    else{
+        log('queue is empty');
+    }
+}
+
+console.log('Adding data in queue')
 enqueue(7);
 enqueue(10);
 enqueue(5);
 enqueue(1);
 enqueue(89);
 displayqueue();
+
+console.log('Deleting data in queue');
+dequeue();
+displayqueue()
