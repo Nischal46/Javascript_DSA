709886f523341d4bb10596f02182221380d46365
Nischal <baniyanisal12@gmail.com>
2024-02-07 22:45:50 +0545

    adding loop to insert dofferent data in queue

--- 02_February/Day_07_Feb_2024.js
+++ 02_February/Day_07_Feb_2024.js
@@ -14,13 +14,15 @@ const operation = +input.question("Enter a operation that needs to perform: ");
 switch(operation){
     case 1:
         log("Adding Data in queue");
-        const val = +input.question("Enter the data that need to added in queue: ")
-        if(queueSize < maxsize){
-            queue[queueSize] = val;
-            queueSize++;
-        }
-        else{
-            log('Queue already full')
+        for(let i=0; i<maxsize; i++){
+            const val = +input.question("Enter the data that need to added in queue: ")
+            if(queueSize < maxsize){
+                queue[i] = val;
+                queueSize++;
+            }
+            else{
+                log('Queue already full')
+            }
         }
         log('Adding data in queue ', queue);
         break;
