960e00ba4fa604ff856e7f33a5e83500341e1fa5
Nischal <baniyanisal12@gmail.com>
2024-02-16 22:23:51 +0545

    using getter and setter by class and object

--- 02_February/Day_16_Feb_2024.js
+++ 02_February/Day_16_Feb_2024.js
@@ -21,3 +21,25 @@ const st1 = new StudentDetails('Nischal', 'baniyanisal12@gmail.com', 'abc123');
 console.log(st1);
 
 console.log(st1.password);
+
+
+class User{
+    constructor(username, password){
+    this.username = username;
+    this.password = password;
+    }
+
+    get username(){
+        return this._username + '@123'
+    }
+
+    //inorder to get the value we must declare the setter function
+
+    set username(value){
+        this._username = value
+    }
+}
+
+const user1 = new User('nisal', 'admin');
+console.log(user1);
+console.log(user1.username);
