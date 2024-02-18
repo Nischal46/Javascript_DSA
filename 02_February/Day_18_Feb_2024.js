1d57b90213c06131e06e1a586c18b62ffebbc07c
Nischal <baniyanisal12@gmail.com>
2024-02-18 23:28:13 +0545

    delete data in array by the filter method and also eliminate the numbers by applied specific condition in array

--- 02_February/Day_18_Feb_2024.js
+++ 02_February/Day_18_Feb_2024.js
@@ -16,4 +16,28 @@ console.log(details1);
 const details2 = new Details('iphone', 2023);
 console.log(details2);
 
+//algorithm for the deletion of the data in array
+
+let arr = [56, 89, 92, 44];
+
+//to have modified of the array, it must be declared by let or var.
+//incase we use const then we cannot mutate the actual array
+//using builtin filter array method delete the data inside of array
+arr = arr.filter(item => item !== 89);
+console.log(arr);
+
+
+let newarray = [34, 65, 33, 21, 63, 4, 78];
+let greaterthan40 = []
+
+//delete number that are lesser than 40
+
+for(let i = 0; i < newarray.length; i++){
+    if(newarray[i] > 40){
+        console.log(`${newarray[i]} is greater than 40`);
+        greaterthan40.push(newarray[i])
+    }
+}
+console.log('number greater than 20 are ', greaterthan40);
+
 
