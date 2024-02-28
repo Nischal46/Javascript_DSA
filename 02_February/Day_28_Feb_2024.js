78957a955b98380a642aa340ea69f722b381b905
Nischal <baniyanisal12@gmail.com>
2024-02-28 22:42:39 +0545

    using some of the useful string methods and make logic of string with the usecase of the array such as split method, join method and other

--- 02_February/Day_28_Feb_2024.js
+++ 02_February/Day_28_Feb_2024.js
@@ -24,3 +24,61 @@ for(let i = 0; i<input.length; i++){
 
 console.log('The repeatance of the array is ', input);
 console.log(`After removing the duplicate value in array `, uniquearray);
+
+//3. string methods
+
+const stringreesult = 'learning javascript concepts'
+//checking the index and the last index of the string
+
+console.log(stringreesult.indexOf('e'));
+console.log(stringreesult.lastIndexOf('e'));
+
+//cut or slicing the string
+console.log(stringreesult.slice(0, 8));
+
+//counting thee index is not ideal and good practice instead we can use this as
+
+console.log(stringreesult.slice(0, stringreesult.indexOf(" ")));
+
+//incase if we want to extract the last word of the string theen
+console.log(stringreesult.slice(stringreesult.lastIndexOf(' ')));
+
+//changing lowercase and uppercase
+console.log(stringreesult.toUpperCase());
+
+//capitalizing the first leetter of the string
+
+const username = 'nischal'
+const finalusername = username[0].toUpperCase() + username.slice(1);
+console.log(finalusername);
+
+//lowercase is mustly used for the email where user may type capital letter and trim is used for removing the white space
+
+const email = '  Nischal@Gmail.com'
+console.log(email);
+
+const correctEmail = email.toLowerCase().trim()
+console.log(correctEmail);
+
+//replace method in string
+
+const str = 'watsup! Buddy'
+console.log(str.replace('watsup', 'Hello'));
+
+const str2 = 'Ram is a good boy. Ram is very dedicated towards his work'
+console.log(str2.replaceAll('Ram', "he"));
+
+//join methid is used to convert array into string and split is opposite
+
+const ConvertingIntoCorrectNameCase = (naminput) => {
+    const name = naminput.split(" ");
+    const newarraynamestore = []   
+    for(const n of name){
+        newarraynamestore.push(n[0].toUpperCase()+n.slice(1))
+    }
+
+    console.log(newarraynamestore.join(" "));
+}
+
+ConvertingIntoCorrectNameCase("nischal baniya")
+ConvertingIntoCorrectNameCase("mr amar singh thapa")
