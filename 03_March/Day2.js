1af98de3c2fa0bb941f28d4b184d9ffd8b434de1
Nischal <baniyanisal12@gmail.com>
2024-03-02 20:38:03 +0545

    solving fibonacci series, factorial number and power of 2.

---
+++ 03_March/Day2.js
@@ -0,0 +1,43 @@
+//algorithm function to check the poweer of Two
+
+function isPowerofTwo(n){
+    if(n < 1) return false; //immediately return the false if the n valuee is less than 1
+
+    while(n > 1){
+        if(n%2 !== 0) return false;
+        console.log('value of n is', n);
+        n = n / 2;
+    }
+
+    return true
+}
+
+console.log(isPowerofTwo(1));
+console.log(isPowerofTwo(3));
+console.log(isPowerofTwo(4));
+console.log(isPowerofTwo(7));
+
+//check the n no of factorial
+
+function checkFactorialNum(n){
+    let result = 1;
+    for(let i = 2; i <= n; i++){
+        result = result * i;
+    }
+
+    return result
+}
+
+console.log(checkFactorialNum(4));
+
+//check fibonacci series
+function fibonacciSeries(n){
+    const fib = [0, 1];
+    for(let i = 2; i <= n; i++){
+        fib[i] = fib[i-2] + fib[i-1]
+    }
+    return fib
+}
+console.log(fibonacciSeries(5));
+
+
