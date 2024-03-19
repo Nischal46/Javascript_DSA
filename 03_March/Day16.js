f7f3ebc29e0f5348027c9d0398b0f775991aac2b
Nischal <baniyanisal12@gmail.com>
2024-03-19 22:00:18 +0545

    difference between the for in and for of loop and make a program that eliminates the white space in the string and count the length of the character in the string

--- 03_March/Day16.js
+++ 03_March/Day16.js
@@ -22,3 +22,28 @@ for(const char of strings){
 }
 console.log('Original string with spaces ', strings);
 console.log('After removing spaces ', newstring);
+
+//count the whitespace and letter in strings
+
+const sentence = "Guitar is the instrument which i liked the most";
+let count = 0;
+let totalcharacter = 0
+for(const char of sentence){
+    if(char === " "){
+        count = count + 1
+    }
+    else{
+        totalcharacter = totalcharacter + 1;
+    }
+}
+console.log('The total whitespace in the string is ', count);
+console.log('The total character in the string is ', totalcharacter); 
+
+//difference between the for of loop and for in loop is that for of loop access the direct value
+//and for in access the key
+
+let stringnew = "hello programmers";
+
+for(const i in stringnew){
+    console.log(i, stringnew[i])
+}
