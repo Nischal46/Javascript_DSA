8fdaf4b90dbdfaa63cc032a91bcfb192c809f12b
Nischal <baniyanisal12@gmail.com>
2024-03-24 22:37:27 +0545

    perform simple logic inoder to seperate the student in multidimensional array using loop iteration and display result as final

--- 03_March/Day24.js
+++ 03_March/Day24.js
@@ -6,24 +6,49 @@ let evenNumbers = [2,4,6,8,10,12,14]
 
 evenNumbers.shift()
 //this shift method change the index of the element in the array
-console.log(evenNumbers);
+// console.log(evenNumbers);
 
 //similarly if we want to restore the value in the first index of the array then
 evenNumbers.unshift(0)
-console.log(evenNumbers);
+// console.log(evenNumbers);
 
 //to delete the element from the last index of the array
 evenNumbers.pop()
-console.log(evenNumbers);
+// console.log(evenNumbers);
 
 //to check the element index in the array
-console.log(evenNumbers.indexOf(12));
+// console.log(evenNumbers.indexOf(12));
 // if the element is preesent in the array then it display the indesx iof the array if not then it gives 
 // -1 as result
 
 //another useful method of the arry is include method tocheck either the data is perviously present or not
 let array = [12, 43, 65, 76, 47, 87];
-console.log(array.includes(49));
+// console.log(array.includes(49));
 
 //another useful method is reverse
-console.log(array.reverse());
+// console.log(array.reverse());
+
+//multi dimensional array : it is array inside array. it can be nested
+
+//suppose there is scenario where the loop iterates for 5 times and each iteration the loop
+// ask to enter the student name and the grade/ If the student is in grade 11 then it is kept
+// in seperate and similar concept for the grade 12
+const input = require('readline-sync');
+let gradearray = [[],[]];
+
+for(let i = 0; i < 5; i++){
+    const name = input.question("Enter student name: ");
+    const grade = +input.question("Enter grade: ");
+
+    if(grade === 11){
+        gradearray[0].push({name, grade})
+    }
+
+    else{
+        gradearray[1].push({name, grade})
+    }
+}
+
+//in other concept we can also used multi dimnensional array inorder to sepearte according to gender
+
+console.log(gradearray);
