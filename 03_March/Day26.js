aa70fc59280012d15ebd5a08b20badd877517ddb
Nischal <baniyanisal12@gmail.com>
2024-03-26 21:14:31 +0545

    reverse the string using the for loop

---
+++ 03_March/Day26.js
@@ -0,0 +1,11 @@
+//reverse a string using logic and buitin method
+
+let st_name = 'nischal baniya'
+let reverse_string = ""
+
+for(let i = st_name.length - 1; i >= 0; i--){
+    reverse_string += st_name[i]
+}
+console.log(reverse_string);
+
+
