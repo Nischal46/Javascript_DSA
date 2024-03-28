457528edc8d068d165697b2ce140b9757be784c1
Nischal <baniyanisal12@gmail.com>
2024-03-28 22:29:43 +0545

    installing mysql db in our project and successfully connect through it and insert and select the data from db

---
+++ 03_March/Day28.js
@@ -0,0 +1,22 @@
+const {createPool} = require('mysql');
+const dotenv = require('dotenv').config({path: '../.env'})
+
+const pool = createPool({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+});
+
+//getting data from the database 
+pool.query('select * from blog', (err, result, fields) => {
+    if(err) return console.log(err);
+    console.log(fields);
+    return console.log(result);
+})
+
+//adding of the data in database
+// pool.query("insert into blog (activity, username, email, date) values ('Myagdi is the largest district', 'saroj bhusal', 'saroj@gmail.com', '2024-03-02')", (err, res) => {
+//     if(err) return console.log(err);
+//     return console.log(res);
+// })
