fe29dafcd11d75db32c30343572694e3defea3fe
Nischal <baniyanisal12@gmail.com>
2024-04-03 22:00:02 +0545

    promise handling

--- 04_April/Day3.js
+++ 04_April/Day3.js
@@ -8,3 +8,16 @@ function CheckPrimeOrNot(arr){
 }
 
 CheckPrimeOrNot([23, 44, 1, 3, 4, 5, 6, 7, 8, 9, 11, 13])
+
+//promise handling
+
+const promise = new Promise((resolve, reject) => {
+    if(5+3 === 8){
+        resolve('promise success');
+    }
+    else{
+        reject('promise reject')
+    }
+});
+
+promise.then((msg) => console.log(msg)).catch((err) => console.log(err));
