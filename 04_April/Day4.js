c61e68330995a1dca050c36e9783823bb80c45e6
Nischal <baniyanisal12@gmail.com>
2024-04-04 21:43:55 +0545

    Factorial series of n number

--- 04_April/Day4.js
+++ 04_April/Day4.js
@@ -10,4 +10,17 @@ function FibonacciSeries(n){
     return arr;
 }
 
-console.log(FibonacciSeries(4))
+console.log(FibonacciSeries(4)) 
+
+function FactorialNumber(n){
+
+    let result = 1;
+
+    for(let i = 2; i <= n; i++){
+        result = result * i;
+    }
+
+    return result;
+}
+
+console.log(FactorialNumber(4));
