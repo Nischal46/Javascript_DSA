143e423361cfe960907991e095da9d18db91eae2
Nischal <baniyanisal12@gmail.com>
2024-04-04 22:02:07 +0545

    adding element in array in nth place

--- 04_April/Day4.js
+++ 04_April/Day4.js
@@ -24,3 +24,19 @@ function FactorialNumber(n){
 }
 
 console.log(FactorialNumber(4));
+
+let array = [12,23,34,54,65,76,44]
+
+function AddElement(n){
+    for(let i = array.length - 1; i >= 0; i--){
+        if(i >= n){
+            array[i+1] = array[i]
+            if(i === n){
+                array[i] = 48
+            }
+        }
+    }
+    return array;
+}
+
+console.log(AddElement(3));
