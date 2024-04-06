894bacfec2dbc1da0b8454611187651c4874bd3f
Nischal <baniyanisal12@gmail.com>
2024-04-06 22:18:07 +0545

    reduce function implement and its use cases for logic building

--- 04_April/Day6.js
+++ 04_April/Day6.js
@@ -23,6 +23,42 @@ class Area extends Reactangle{
     }
 }
 
-const obj = new Area(4, 5);
-obj.area();
-obj.getDetails();
+// const obj = new Area(4, 5);
+// obj.area();
+// obj.getDetails();
+
+
+//using the flatmap concept
+
+const data = [1,2, [5,6], [7,8, [12, 14]]]
+console.log(data);
+const singlearray = data.flat(2)
+console.log(singlearray);
+// using the flat method we can dive deeper
+
+const flatmap = data.flatMap((pd) => pd);
+console.log(flatmap);
+//using the flatmap concept we can dive onto only 1 state.
+
+const numarray = [1,2,3,4,5,6,7,8,9];
+
+const num1 = numarray.reduce((acc, pd) => {
+    if(pd > 5) return [...acc, pd]
+    else return acc
+}, [])
+
+console.log(num1);
+
+const studentarray = [
+    {name: 'nischal', faculty: 'IT'},
+    {name: 'parina', faculty: 'BBA'},
+    {name: 'pradip', faculty: 'MBA'},
+    {name: 'suresh', faculty: "IT"}
+]
+
+const it_array = studentarray.reduce(function(acc, cur){
+    if(cur.faculty === "IT") return [...acc, cur];
+    else return acc;
+}, []);
+
+console.log(it_array);
