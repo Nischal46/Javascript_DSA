d598a6838d15ac1701acd2b6e87afa33e10e66ff
Nischal <baniyanisal12@gmail.com>
2024-04-07 21:31:19 +0545

    Single inheritence and the usage of the parent class method to its child class

---
+++ 04_April/Day7.js
@@ -0,0 +1,25 @@
+class Instruments{
+    constructor(name, model, price){
+        this.name = name;
+        this.model = model;
+        this.price = price
+    }
+
+    getdetails(){
+       return `Name: ${this.name}, Model: ${this.model}`;
+    }
+}
+
+class SubInstruments extends Instruments{
+    constructor(name, model, price){
+        super(name, model, price)
+    }
+
+    getMRPDetails(){
+        const output = (this.price * 13) / 100;
+        console.log(`${this.getdetails()} and the total MRP price is ${output}`);
+    }
+}
+
+const obj1 = new SubInstruments('Ukulele', 'X2345', 2500);
+obj1.getMRPDetails();
