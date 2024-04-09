5d9a188d2f589fb9c40c6745154996cd972c1d12
Nischal <baniyanisal12@gmail.com>
2024-04-09 20:50:36 +0545

    adding of the element in the array by using both logic and built in method

--- 04_April/Day9.js
+++ 04_April/Day9.js
@@ -11,4 +11,29 @@ function AddingNumber(num){
     return total
 }
 
-console.log(AddingNumber(23456));
+// console.log(AddingNumber(23456));
+
+//adding, deleting, updating and searching of the data in array
+
+let array = [32, 43, 56, 88, 26, 83];
+
+function AddingElement(newelement, position){
+    for(let i = array.length - 1; i >= 0; i--){
+        if(i >= position){
+            array[i+1] = array[i]
+
+            if(i === position){
+                array[i] = newelement
+            }
+        }
+    }
+
+    return array
+}
+
+// console.log(AddingElement(77, 2));
+
+//alternative splice method
+
+array.splice(2, 0, 77);
+console.log(array);
