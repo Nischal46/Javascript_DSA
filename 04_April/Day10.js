40da84c292a86c8a1e0f4c40eeba5de4f7469c99
Nischal <baniyanisal12@gmail.com>
2024-04-10 22:18:04 +0545

    seperate the array according to the divider of 2 and 3

--- 04_April/Day10.js
+++ 04_April/Day10.js
@@ -19,3 +19,19 @@ const sum_Square_Total = original_array.reduce(function(acc, elem){
     return acc + (elem*elem)
 }, 0)
 console.log(sum_Square_Total);
+
+let array =  [12, 15, 16, 18, 21, 25];
+
+const seperate_by_divisor = array.reduce(function(acc, cur){
+    if(cur % 2 === 0){
+        acc.push(`${cur} is divide by 2`);
+    }
+
+    if(cur % 3 === 0){
+        acc.push(`${cur} is divided by 3`);
+    }
+
+    return acc
+}, [])
+
+console.log(seperate_by_divisor);
