c8ebc2f7d846234cc9c8f5f91c253445b1540c0d
Nischal <baniyanisal12@gmail.com>
2024-04-10 22:32:20 +0545

    solve the question that takes array of different string as function arguement and check all the string whether palindrome or not in each iteration

--- 04_April/Day10.js
+++ 04_April/Day10.js
@@ -1,12 +1,12 @@
 //write a function that calculates the sum of square of all the elements
 
-let original_array = [2,4,5]
+let original_array = [2, 4, 5]
 let total = 0;
 //output is expected as 4 + 16 + 25 = 45
 
-function Square_Sum(arr){
-    for(const i of arr){
-        total = total + (i*i)
+function Square_Sum(arr) {
+    for (const i of arr) {
+        total = total + (i * i)
     }
     return total
 }
@@ -15,23 +15,55 @@ function Square_Sum(arr){
 
 //alternative method
 
-const sum_Square_Total = original_array.reduce(function(acc, elem){
-    return acc + (elem*elem)
+const sum_Square_Total = original_array.reduce(function (acc, elem) {
+    return acc + (elem * elem)
 }, 0)
 console.log(sum_Square_Total);
 
-let array =  [12, 15, 16, 18, 21, 25];
+let array = [12, 15, 16, 18, 21, 25];
 
-const seperate_by_divisor = array.reduce(function(acc, cur){
-    if(cur % 2 === 0){
+const seperate_by_divisor = array.reduce(function (acc, cur) {
+    if (cur % 2 === 0) {
         acc.push(`${cur} is divide by 2`);
     }
 
-    if(cur % 3 === 0){
+    if (cur % 3 === 0) {
         acc.push(`${cur} is divided by 3`);
     }
 
     return acc
 }, [])
 
-console.log(seperate_by_divisor);
+// console.log(seperate_by_divisor);
+
+//write a function that checks the given string is palindrome or not. Pass the array of multiple string value by use of function
+
+
+function Check_Palindrome(str_arr) {
+
+    for (const cd of str_arr) {
+        let length = cd.length - 1;
+        let newstring = ""
+
+        while (length >= 0) {
+            newstring += cd[length];
+            length--;
+        }
+
+        if (cd === newstring) {
+            console.log(`${cd} is a palindrome string`);
+        }
+    
+        else {
+            console.log(`${cd} is not a palindrome string`);
+        }
+        newstring = "";
+    }
+
+}
+
+let array_String = ['wow', 'racecar', 'songs']
+
+Check_Palindrome(array_String)
+
+
