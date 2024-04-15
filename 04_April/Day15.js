dafae88e3451fd7eeda2f1c3cafe56c1ed2c96a3
Nischal <baniyanisal12@gmail.com>
2024-04-15 23:12:05 +0545

    implement simple program logic using control flow of switch condition and while condition when adding the product detials

--- 04_April/Day15.js
+++ 04_April/Day15.js
@@ -11,11 +11,53 @@ let role_verified = ['admin'];
 
 let product_array = [];
 
-for (let i = 0; i < 5; i++) {
-    const product_name = input.question("Enter a product name: ");
-    const product_price = +input.question("Enter a product price: ");
+// for (let i = 0; i < 5; i++) {
+//     const product_name = input.question("Enter a product name: ");
+//     const product_price = +input.question("Enter a product price: ");
 
-    product_array.push({product_name, product_price});
+//     product_array.push({product_name, product_price});
+// }
+
+// console.log(product_array);
+
+function display_option(){
+    console.log(`
+    1. Add product
+    2. check product
+    3. quit
+    `);
 }
+let bool = true;
 
-console.log(product_array);
+while(bool){
+    display_option();
+    const option = +input.question("Enter a choice: ");
+
+    switch(option){
+        case 1:
+            const product_name = input.question("Enter a product name: ");
+            const product_price = +input.question("Enter a product price: ");
+
+            product_array.push({product_name, product_price});
+
+            break;
+
+        case 2:
+            if(product_array.length < 1) console.log('Product array is empty.');
+
+            else {const detail = product_array.map((cl, i) => (
+                console.log(`${i} ${cl.product_name}`)
+            ))}
+
+            break;
+
+        case 3:
+            bool = false;
+            break;
+
+        default:
+            console.log('Invalid input. Please choice correct input.');
+            break;
+
+    }
+}
