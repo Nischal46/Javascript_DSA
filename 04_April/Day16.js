4992c124dba894204b911d8407eca08d20efbce0
Nischal <baniyanisal12@gmail.com>
2024-04-16 22:35:08 +0545

    function that calculate the median of the numbers

--- 04_April/Day16.js
+++ 04_April/Day16.js
@@ -14,4 +14,35 @@ function Calculate_Mean(arr_num){
     console.log('The final result of mean is ', result);
 }
 
-Calculate_Mean(numbers)
+// Calculate_Mean(numbers)
+
+//write a function that calculate the median of the number data
+
+let number = [12, 43, 33, 27, 78, 65];
+
+function Calculate_Median(arr){
+    //first of all arrange data in ascending order
+
+    let sort_data = arr.sort((a,b) => a-b)
+    // console.log(sort_data);
+
+    const n = sort_data.length - 1;
+
+    const result = (n / 2);
+
+    // console.log(Math.floor(result));
+
+    let output;
+
+    if((Math.floor(result)) < result){
+        output = (arr[Math.floor(result)] + arr[Math.floor(result) + 1]) / 2;
+    }
+
+    else{
+        output = arr[result];
+    }
+
+    console.log('The median is ', output);
+}
+
+Calculate_Median(number)
