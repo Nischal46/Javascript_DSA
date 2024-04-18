c7a31817a9c3b7636e8cfb5aafb862046cc8ed41
Nischal <baniyanisal12@gmail.com>
2024-04-18 22:59:11 +0545

    function that avoid negative number and sum positive number and gives output

--- 04_April/Day18.js
+++ 04_April/Day18.js
@@ -17,3 +17,19 @@ function countRepeatance(arr) {
 let array = [12, 23, 42, 12, 54, 23, 23, 65, 76];
 let result = countRepeatance(array);
 console.log(result);
+
+//write a function that only sum the positive number and avoid negative number in array
+
+function sumOfPositive(arr) {
+    let sum = 0;
+    for (const num of arr) {
+        if (num > 0) {
+            sum += num;
+        }
+    }
+    return sum;
+}
+
+let array2 = [-1, 2, -3, 4, -5, 6];
+console.log(sumOfPositive(array2)); // Output: 12
+
