79e0bc8c70691f605be2b45346405ab13eafbb8e
Nischal <baniyanisal12@gmail.com>
2024-04-19 23:18:03 +0545

    bubble sort to arrane sort of array data

--- 04_April/Day19.js
+++ 04_April/Day19.js
@@ -11,3 +11,23 @@ function fibonacci_Series(no){
 }
 
 console.log(fibonacci_Series(7));
+
+//write a function that display factorial number
+
+function Sorting_function(arr){
+
+    for(let i = 0; i <= arr.length - 1; i++){
+        for(let j = 0; j <= arr.length - 1; j++){
+            if(arr[j] > arr[j+1]){
+                let temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+
+    return arr;
+
+}
+
+console.log(Sorting_function([23,12,4,8,9,56,35]));
