7aeb0d20322938add1f9fe99ea1d64236f0fd6f0
Nischal <baniyanisal12@gmail.com>
2024-04-20 22:29:18 +0545

    using flat and flatmap concept in array and differentiate thie working

--- 04_April/Day20.js
+++ 04_April/Day20.js
@@ -13,5 +13,24 @@ class SubClass extends BaseClass{
     }
 }
 
-const obj1 = new SubClass('nischal', 2021, 'lalitpur', 'IT professor');
-console.log(obj1);
+// const obj1 = new SubClass('nischal', 2021, 'lalitpur', 'IT professor');
+// console.log(obj1);
+
+let innerArray = [12, 43, 56, [42, 56, 76]];
+
+
+//flatmap is to eleimnate and store the data in single array if there is existed of inner array.
+//this works for only one nested array.
+
+const singleArray = innerArray.flatMap(cl => cl);
+console.log(singleArray);
+
+let innerarray2 = [23, 54, 65, 33, [89, 90, 12, [34, 66]]]
+
+// const newarray = innerarray2.flatMap(cl => cl);
+
+// console.log(newarray);
+
+const flatarray = innerarray2.flat(2);
+console.log(flatarray);
+
