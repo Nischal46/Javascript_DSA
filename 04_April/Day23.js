073c3ab351ae2abcfd19fa0a4584005a21bda2ce
Nischal <baniyanisal12@gmail.com>
2024-04-23 22:30:22 +0545

    arranging data in descending order using bubble sort algorithm

--- 04_April/Day23.js
+++ 04_April/Day23.js
@@ -13,3 +13,17 @@ for (let i = numArray.length - 1; i >= 0; i--){
 }
 
 console.log(numArray);
+
+//arranging data in descending order by using bubble sort algorithm
+
+for(let i = 0; i <= numArray.length - 1; i++){
+    for(let j = 0; j <= numArray.length - 1; j++){
+        if(numArray[j] < numArray[j + 1]){
+            let temp = numArray[j];
+            numArray[j] = numArray[j+1];
+            numArray[j+1] = temp;
+        }
+    }
+}
+
+console.log(numArray);
