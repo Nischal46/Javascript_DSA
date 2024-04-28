66a58f8e60a98e7a7c1de21a74cf7bd4b71c02c3
Nischal <baniyanisal12@gmail.com>
2024-04-28 22:16:44 +0545

    make a program that identifies the number in array is fizz buzz

--- 04_April/Day28.js
+++ 04_April/Day28.js
@@ -19,3 +19,17 @@ class Vehicle{
 const obj = new Vehicle('Tesla', 'brand');
 
 console.log(obj.static());
+
+//write a program to print fizzbuzz concept where num divided by 3 is fizz, num divided by 5 is buzz and if both then it is fizzbuzz
+
+let numArray = [12, 15, 21, 25, 45];
+
+function fizzBuzz(arr){
+    for(const no of arr){
+        if(no % 3 === 0 && no % 5 === 0) console.log(`${no} is fizzbuzz`);
+        else if(no % 3 === 0) console.log(`${no} is fizz`);
+        else  if(no % 5 === 0) console.log(`${no} is buzz`);
+    }
+}
+
+fizzBuzz(numArray)
