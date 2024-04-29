8afdde9e4b8bd0d7b9bd43ca2a93909d97059e4b
Nischal <baniyanisal12@gmail.com>
2024-04-29 22:11:14 +0545

    make a simple program that response the user input email address authentic or not by promise handling method

--- 04_April/Day29.js
+++ 04_April/Day29.js
@@ -16,4 +16,21 @@ function CheckPalindrome(arr){
 
 let strArray =['racecar', 'pop', 'loop', 'wow', 'pen'];
 
-CheckPalindrome(strArray)
+// CheckPalindrome(strArray)
+
+//Promise resolve and reject
+
+//using promise check the user input email address is authentic or not
+
+function ValidateEmailAddress(input){
+    return new Promise((resolve, reject) => {
+        if(input.includes("@") && input.includes(".")) resolve('verified');
+        else reject('unverified')
+    })
+}
+
+ValidateEmailAddress('baniyanisal12@gmail.com')
+.then((res) => {
+    console.log("Email Address verified successfully");
+}).catch((err) => console.log('Email Address verified failed')
+)
