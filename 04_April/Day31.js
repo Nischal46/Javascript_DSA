3bc8fde6fafcc24ca6d4fe5bf5eff9bff490c375
Nischal <baniyanisal12@gmail.com>
2024-05-01 22:16:34 +0545

    promise handling concept by building call logic system

--- 04_April/Day31.js
+++ 04_April/Day31.js
@@ -24,3 +24,24 @@ const sepName = array.reduce((acc, cur) => {
 }, [])
 
 console.log(sepName);
+
+
+//promise handling
+
+function CallingLogic(number){
+
+    let no = String(number)
+    // console.log(no, typeof no);
+    // console.log(no.length);
+
+    return new Promise((resolve, reject) => {
+        if(no.length >= 11) reject("Number is not 10 digits")
+
+        else resolve("Successfull")
+    })
+}
+
+CallingLogic(9846874323)
+.then((res) => console.log('Call is dailing'))
+.catch((err) => console.log(`Call is rejected. Reason: ${err}`))
+
