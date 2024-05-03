ec8dc65e94485f4585acb30380a52d6107810651
Nischal <baniyanisal12@gmail.com>
2024-05-03 22:21:52 +0545

    Perform assync await handling successfully

--- 05_May/Day3.js
+++ 05_May/Day3.js
@@ -11,3 +11,27 @@ function checkGreater(a, b){
 checkGreater(6, 4)
 .then((res) => console.log(`A is greater than B`))
 .catch((err) => console.log(`A is smaller than B`))
+
+
+// async await in js
+
+async function Entry(name, email){
+    let userName, userEmail;
+    if(!name.includes(' ')){
+        return 'User must include full name';
+    }
+    else userName = name;
+
+    if(!email.includes('@') || !email.includes('.')){
+        return 'User must include tha valid email address'
+    }
+
+    else userEmail = email
+
+    return await [userName, userEmail];
+}
+
+Entry('Nischal Baniya', 'nisal@gmail.com')
+.then((res) => console.log(`Result obtained: ${res}`))
+.catch((err) => console.log('Error caught'))
+.finally(x=> console.log('Perform async await successfully'));
