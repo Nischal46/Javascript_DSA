d83c7699808231d5a892045614a30bbea4e6b76c
Nischal <baniyanisal12@gmail.com>
2024-05-08 21:37:46 +0545

    using splice in array method

--- 05_May/Day8.js
+++ 05_May/Day8.js
@@ -44,3 +44,18 @@ const convertObjectToArray = Object.keys(studentdetails); // this return only su
 
 
 console.log(convertObjectToArray);
+
+const studentArray =['hari', 'om', 'jagdish'];
+studentArray.splice(0,0,'ram');
+console.log(studentArray);;
+//for the deletion of the data ion array
+
+let details = ['engineer', 'doctor', 'pilot', 'lawyer'];
+details.sort((a,b) => a.localeCompare(b));
+
+console.log(details);
+
+const details1 = [12,43,54,656,777];
+
+details1.splice(0, 1);
+console.log(details1);
