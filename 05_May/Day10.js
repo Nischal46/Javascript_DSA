976c50ca6cb1a142ee9d1ee594f34830c81c38c8
Nischal <baniyanisal12@gmail.com>
2024-05-10 21:36:31 +0545

    async await handle in registration of the user program logic

--- 05_May/Day10.js
+++ 05_May/Day10.js
@@ -14,6 +14,20 @@ function AdminLoginSystem(email, password){
     
 }
 
-AdminLoginSystem('admin@gmail.com', 'admin1')
-.then((res) => console.log('Successful Login'))
-.catch((err) => console.log('Login failed'))
+// AdminLoginSystem('admin@gmail.com', 'admin1')
+// .then((res) => console.log('Successful Login'))
+// .catch((err) => console.log('Login failed'))
+
+// alternative way async await
+
+async function RegistrationSystem(name, email, password){
+    if(name === "") throw await "Name should not be empty"
+    else if(!email.includes('.') || !email.includes('@')) throw await "Email should be valid"
+    else if(!password < 8) throw await "Password length must be greater than eight character"
+
+    else return await "Successful registration"
+}
+
+RegistrationSystem('Nischal', 'nisal@gmail.com', 'passwor')
+.then((res) => console.log('Successful registration'))
+.catch((err) => console.log(`Failed registration: ${err}`))
