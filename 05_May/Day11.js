d0d99e71cbd5aeee2df0ad151124b49772010aaa
Nischal <baniyanisal12@gmail.com>
2024-05-11 22:51:57 +0545

    class and object OOP

---
+++ 05_May/Day11.js
@@ -0,0 +1,27 @@
+class Songs{
+    constructor(songName, songDuration, songType){
+        this.songName = songName;
+        this.songDuration = songDuration;
+        this.songType = songType;
+    }
+}
+
+class SongsPrototype extends Songs{
+    constructor(songName, songDuration, songType){
+        super(songName, songDuration, songType)
+    }
+
+    getDetails(){
+        console.log(
+            `
+            song Name: ${this.songName}
+            song Duration: ${this.songDuration}
+            song Type: ${this.songType}
+            `
+        );
+    }
+
+}
+
+const obj = new SongsPrototype('Breathless', 3, 'Classical-Rock');
+obj.getDetails();
