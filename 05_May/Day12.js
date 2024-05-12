4d5d27e3877bdc25f4ee1e54ed8fc2a0ee37ff48
Nischal <baniyanisal12@gmail.com>
2024-05-12 22:40:07 +0545

    insertion, deletion in array using loop logic

---
+++ 05_May/Day12.js
@@ -0,0 +1,44 @@
+// insertion, deletion, searching and updating of the array.
+
+let numArray = [12, 43, 22, 67, 53, 37, 88];
+
+for (let i = numArray.length - 1; i >= 0; i--){
+    if(i >= 3){
+        numArray[i+1] = numArray[i];
+        
+        if(i == 3){
+            numArray[i] = 7;
+        }
+    }
+}
+
+console.log(numArray);
+
+//arranging data in descending order by using bubble sort algorithm
+
+for(let i = 0; i <= numArray.length - 1; i++){
+    for(let j = 0; j <= numArray.length - 1; j++){
+        if(numArray[j] < numArray[j + 1]){
+            let temp = numArray[j];
+            numArray[j] = numArray[j+1];
+            numArray[j+1] = temp;
+        }
+    }
+}
+
+console.log(numArray);
+
+let randomNum = [12, 34, 66, 21, 43, 66, 78, 88];
+
+const numDivisible = randomNum.filter(function(cl){
+    if(cl % 2 == 0) console.log(`${cl} divisible by 2`);
+})
+
+console.log(numDivisible);
+
+//differentiate between flat and flatmap
+
+let stname = ['ram', 'laxman', 'dipak', 'aman', 'roshan', ['samir', 'sabin', 'hari', ['omkar']]];
+
+const stdetail = stname.flat(2).sort();
+console.log(stdetail);
