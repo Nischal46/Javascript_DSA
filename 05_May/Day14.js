459e0242bf4b6fe693959d075a83f3e42a7bdb87
Nischal <baniyanisal12@gmail.com>
2024-05-14 23:45:49 +0545

    recursion function to search the data in array and using the loop logic with fn

--- 05_May/Day14.js
+++ 05_May/Day14.js
@@ -151,3 +151,35 @@ while(gameStart){
             break;
     }
 }
+
+// recursion to find the data in array
+
+const searchEelement = 73;
+
+function Binary_Search_Fn(array, searchEelement){
+
+    let left = 0;
+    let right = array.length - 1;
+    
+    while(left <= right){
+        let mid = Math.floor((left + right) / 2);
+
+        if(array[mid] === searchEelement){
+            return mid;
+        }
+
+        if(searchEelement < array[mid]){
+            right = mid -1;
+        }
+
+        else {
+            left = mid + 1;
+        }
+
+    }
+
+    return -1;
+   
+}
+
+const result = Binary_Search_Fn(array, searchEelement);
