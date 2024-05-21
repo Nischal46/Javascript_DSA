044bbe84ef5832977a8ee77141ffcd0d83924015
Nischal <baniyanisal12@gmail.com>
2024-05-21 21:32:32 +0545

    constructor function and prototypal inheritence

--- 05_May/Day21.js
+++ 05_May/Day21.js
@@ -74,3 +74,40 @@ class Rectangle{
 
 const rect_obj = new Rectangle(4, 5).Area();
 console.log(rect_obj);
+
+
+//constructor function
+
+let studentArray = [
+    {id: 1, name: "Ram", degree: 'bachelor', status: 'married', gender: 'male'},
+    {id: 2, name: "Shyam", degree: 'SEE', status: 'unmarried',  gender: 'male'},
+    {id: 3, name: "Hari", degree: 'NEB', status: 'unmarried',  gender: 'male'},
+    {id: 4, name: "Laxman", degree: 'bachelor', status: 'married',  gender: 'male'},
+    {id: 5, name: "Krishna", degree: 'bachelor', status: 'unmarried',  gender: 'male'},
+    {id: 6, name: "Parbati", degree: 'NEB', status: 'unmarried',  gender: 'female'},
+    {id: 7, name: "Laxmi", degree: 'bachelor', status: 'married',  gender: 'female'}
+]
+
+function Student(array, searchInput, searchGender){
+    // this.name = name;
+    this.searchInput = searchInput,
+    this.array = array,
+    this.searchGender = searchGender
+}
+
+Student.prototype.SearchByDegree = function(){
+    this.array = this.array.filter(ex => ex.degree === this.searchInput);
+    return this;
+}
+
+Student.prototype.SearchByGender = function(){
+    this.array = this.array.filter(ex => ex.gender === this.searchGender)
+    return this;
+}
+
+const stobj = new Student(studentArray, "bachelor").SearchByDegree()
+console.log(stobj);
+
+const stobj1 = new Student(studentArray, '',"female").SearchByGender();
+console.log(stobj1);
+
