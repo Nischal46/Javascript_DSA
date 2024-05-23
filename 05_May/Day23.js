d026ab96a5bdc34106f8e8d6976e0a04c8541084
Nischal <baniyanisal12@gmail.com>
2024-05-23 22:00:20 +0545

    flatmap proto concept of array

---
+++ 05_May/Day23.js
@@ -0,0 +1,24 @@
+let moviesarray = [
+    {
+        id: 's123',
+        name: 'kung fu panda 2',
+        type: 'action'
+
+    },
+
+    {
+        id: 's432',
+        name: 'Intestellar',
+        type: 'science-fiction'
+    },
+
+    {
+        id: 's654',
+        name: 'inception',
+        type: "science-fiction"
+    }
+]
+
+let movienamearray = moviesarray.flatMap(cl => cl.name);
+console.log(movienamearray);
+
