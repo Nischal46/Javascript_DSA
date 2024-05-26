92f966e46331db940dd91e8403290187a0e93f1a
Nischal <baniyanisal12@gmail.com>
2024-05-26 22:46:56 +0545

    class and enqueue algorithm

--- 05_May/Day26.js
+++ 05_May/Day26.js
@@ -44,3 +44,65 @@ const convertObjectToArray = Object.keys(studentdetails); // this return only su
 
 
 console.log(convertObjectToArray);
+
+class Student{
+    constructor(name, email, faculty){
+        this.name = name;
+        this.email = email;
+        this.faculty = faculty
+    }
+
+    details(){
+        console.log(`Your name ${this.name}. Email Address ${this.email} and faculty ${this.faculty}`);
+    }
+}
+
+const stobject = new Student('Nischal', 'baniyanisal12@gmail.com', 'IT');
+
+console.log(stobject);
+
+stobject.details();
+
+// queue function
+
+let queue = [];
+
+let maxsize = 4;
+
+let queuesize = queue.length;
+
+function enqueue(val){
+    if(queuesize <= maxsize){
+        queue[queuesize] = val;
+        queuesize++;
+    }
+
+    else{
+        console.log('queue is fulfilled already');
+    }
+}
+
+function displayqueue(){
+    console.log('the current queue is ', queue);
+}
+
+function dequeue(){
+    if(queuesize > 0){
+        for(let i=0; i < queue.length; i++){
+            queue[i] = queue[i+1];
+        }
+        queuesize--;
+        queue.length = queuesize;
+    }
+    else{
+        log('queue is empty');
+    }
+}
+
+enqueue(5);
+enqueue(1);
+displayqueue();
+
+console.log('Deleting data in queue');
+dequeue();
+displayqueue()
