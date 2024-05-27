a3dc9074785ab125afb128f7020dd9e7c480ea59
Nischal <baniyanisal12@gmail.com>
2024-05-27 21:28:08 +0545

    function to remove the duplicate number in array

--- 05_May/Day27.js
+++ 05_May/Day27.js
@@ -3,3 +3,18 @@ let array = [23, 54, 66, 76, 87, 89, 97];
 let removingarray = array.splice(0, 2);
 
 console.log(removingarray);
+
+let num1 = [12, 23, 34, 45, 56, 34, 34, 76, 45];
+
+function removeDuplicate(no){
+    let removeDuplicateNumber = []
+    for(const item of no){
+        if(!removeDuplicateNumber.includes(item)){
+            removeDuplicateNumber.push(item)
+        }
+    }
+
+    return removeDuplicateNumber;
+}
+
+console.log(removeDuplicate(num1));
