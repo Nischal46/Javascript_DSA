eaf056c89e3617331b3dd59343376e70144a1d17
Nischal <baniyanisal12@gmail.com>
2024-05-27 21:48:47 +0545

    identifying the largest and smallest number of the array

--- 05_May/Day27.js
+++ 05_May/Day27.js
@@ -8,11 +8,23 @@ let num1 = [12, 23, 34, 45, 56, 34, 34, 76, 45];
 
 function removeDuplicate(no){
     let removeDuplicateNumber = []
+    let highestnum = 0;
+    let lowestnum = no[0];
+    
     for(const item of no){
+       
         if(!removeDuplicateNumber.includes(item)){
             removeDuplicateNumber.push(item)
         }
+        if(item > highestnum) highestnum = item;
+
+        if(item < lowestnum) lowestnum = item;
+    
     }
+
+    
+    console.log('The largest number in the array is ', highestnum);
+    console.log('The smallest number in the array is ', lowestnum);
 
     return removeDuplicateNumber;
 }
