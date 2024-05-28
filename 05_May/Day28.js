37cca8e5eac9f2bcb95a8e0fb4b6d2c81d4525bb
Nischal <baniyanisal12@gmail.com>
2024-05-28 22:22:19 +0545

    palindrome number

---
+++ 05_May/Day28.js
@@ -0,0 +1,9 @@
+function isPalindrome(str) {
+    let cleanedStr = str.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
+    let reversedStr = cleanedStr.split('').reverse().join('');
+    return cleanedStr === reversedStr;
+  }
+  
+  // Example usage:
+  console.log(isPalindrome("A man, a plan, a canal, Panama")); // Output: true
+  console.log(isPalindrome("hello")); // Output: false
