0cb084491571448fc8546ba26634bae3b7775701
Nischal <baniyanisal12@gmail.com>
2024-05-29 20:32:23 +0545

    constructor function access by object to its prototpe method

--- 05_May/Day29.js
+++ 05_May/Day29.js
@@ -2,7 +2,7 @@
 
 function FizzBuzz(arr){
     for(const cl of arr){
-        if(cl % 3 == 0 || cl % 5 == 0){
+        if(cl % 3 == 0 && cl % 5 == 0){
             console.log(`${cl} is a fizzBuzz number`);
         }
         else if(cl % 3 == 0) console.log(`${cl} is a Fuzz Number`);
@@ -16,3 +16,19 @@ function FizzBuzz(arr){
 
 let noArray = [2,3,5, 10, 15, 20, 25, 35]
 FizzBuzz(noArray)
+
+//constructor function
+
+function ConstructorFunction(name, email){
+    this.name = name;
+    this.email = email;
+    console.log(this);
+}
+
+ConstructorFunction.prototype.greeting = function(){
+    console.log(`Welcome ${this.name} and your email account is ${this.email}`);
+}
+
+// to access the constructor function method peoperties we should declare the object
+const obj = new ConstructorFunction('Nischal', 'baniyanisal@gmail.com')
+obj.greeting();
