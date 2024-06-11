fa8a7c532abdf11c2f43904bf66350f034344099
Nischal <baniyanisal12@gmail.com>
2024-06-11 22:21:08 +0545

    higher order fn usage for the calculation of multiplication table

--- 06_June/Day11.js
+++ 06_June/Day11.js
@@ -12,3 +12,24 @@ function Secondaryfn(){
 }
 
 Mainfn(Secondaryfn);
+
+//multiplication table using the higher order fn
+
+function Multiplication(fnPassed){
+
+    let num = 10;
+    let res = fnPassed();
+    console.log(res);
+
+    for(let i = 1; i <= res; i++){
+        console.log(`${num} * ${i} = ${num * i}`);
+    }
+
+}
+
+let no = 5
+function NoofTimes(){
+    return no;
+}
+
+Multiplication(NoofTimes);
