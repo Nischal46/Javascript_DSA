df3b5c57487bb1fd34a00a7297c5e84e309875b6
Nischal <baniyanisal12@gmail.com>
2024-06-16 21:28:32 +0545

    async await operation

--- 06_June/Day16.js
+++ 06_June/Day16.js
@@ -11,10 +11,26 @@ Login()
 .then((response) => response.json())
 .then(json => {
     let data;
-    data = json.filter(cd => cd.id < 10)
+    data = json.filter(cd => cd.id < 5)
     console.log(data);
 })
 .catch(() => console.log('Error occured'))
 .finally(function(){
     console.log('The api is fetched successfully');
 })
+
+
+async function HandleRequest(){
+    
+    let data;
+
+    const apiRequest = await fetch('https://jsonplaceholder.typicode.com/users');
+    const apiResponse = await apiRequest.json();
+
+    data = apiResponse.filter(cd => cd.id < 10)
+
+    return `from async await ${data}`;
+}
+
+const asyncAwait = HandleRequest();
+console.log(asyncAwait);
