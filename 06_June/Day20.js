e8bc513ee5e0aaa85c12ae3e90f2f1c90751214f
Nischal <baniyanisal12@gmail.com>
2024-06-20 21:40:14 +0545

    bind method in js

--- 06_June/Day20.js
+++ 06_June/Day20.js
@@ -19,3 +19,20 @@ function SIMRegistration(name, address, citizenship) {
 
 const object = new SIMRegistration('Nischal', 'Lalitpur', '789765767');
 console.log(object);
+
+//bind method is to add function or further in specific function
+
+function CheckEligibility(age){
+    if(age > 18) this.eligible = true;
+    else this.eligible = false;
+}
+
+function DrivingManagementSystem(name, age, contact){
+    this.name = name;
+    this.contact = contact;
+    const details = CheckEligibility.bind(this, age);
+    details()
+}
+
+const object1 = new DrivingManagementSystem('Ravi', 42, 9843598532);
+console.log(object1);
