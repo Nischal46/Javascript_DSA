6fec1badd9558f633cab29732d964d3a24b01e3f
Nischal <baniyanisal12@gmail.com>
2024-06-23 21:09:21 +0545

    factory function and its prototypal inheritence

--- 06_June/Day23.js
+++ 06_June/Day23.js
@@ -5,3 +5,21 @@ const stName = ['ramesh', 'laxman', 'hari', 'suman'];
 stName.forEach((cl, ind) => {
     console.log(`Student no ${ind + 1}:-> ${cl}`);
 });
+
+//constructor function and its prototypal inheritence
+
+
+function Categories(name, type){
+    this.name = name;
+    this.type = type;
+    }
+    
+    Categories.prototype.details = function(){
+        if(this.type === "mp3") console.log('This is the music file of the songs');
+        else if(this.type === "mp4") console.log('This is the video file of the songs');
+    }
+    
+    const obj = new Categories('Hukka mero', 'mp3');
+    console.log(obj);
+    obj.details();
+    
