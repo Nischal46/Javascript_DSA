847f7545a7ced11090549676cc08c63e996f9f85
Nischal <baniyanisal12@gmail.com>
2024-06-28 18:59:39 +0545

    Object shallow copy and deep copy and using of lodash library

--- 06_June/Day28.js
+++ 06_June/Day28.js
@@ -62,27 +62,77 @@ function *findUser(){
     yield dbArray[Math.floor(Math.random() * dbArray.length)].name;
 }
 
-const callsetinterval = setInterval(() => {
-    const fncallName = findUser();
-    const nameresponse = fncallName.next().value;
-    const nameToBeSearched = "nischal";
+// const callsetinterval = setInterval(() => {
+//     const fncallName = findUser();
+//     const nameresponse = fncallName.next().value;
+//     const nameToBeSearched = "nischal";
 
-    if(nameresponse === nameToBeSearched){
-        console.log('Name found successfully!');
-        console.log(`
-            Name to be search: ${nameToBeSearched} || Name found by query: ${nameresponse}`);
-        clearInterval(callsetinterval);
-        return;
-    }
+//     if(nameresponse === nameToBeSearched){
+//         console.log('Name found successfully!');
+//         console.log(`
+//             Name to be search: ${nameToBeSearched} || Name found by query: ${nameresponse}`);
+//         clearInterval(callsetinterval);
+//         return;
+//     }
 
-    else{
-        console.log('Searching......');
-        console.log(`
-            Name to be search: ${nameToBeSearched} || Name found by query: ${nameresponse}`);
-    }
-}, 3000)
+//     else{
+//         console.log('Searching......');
+//         console.log(`
+//             Name to be search: ${nameToBeSearched} || Name found by query: ${nameresponse}`);
+//     }
+// }, 3000)
 
 // console.log(dbArray[Math.floor(Math.random() * dbArray.length)].name);
+
+//Object Deep Copy and Shallow copy
+
+let objectInitial = {
+    name: 'Nischal',
+    email: 'nisal@gmail.com',
+    profession: 'Senior IT Engineer'
+}
+
+// let objectCopy = objectInitial; //simple copy of the memory location
+
+//shallow copy
+//can create in two way that is either using destructure or Object.assign
+//they also have disadvantage because it only works for one level and doesnot work for nested
+
+// let objectCopy = {...objectInitial}
+let objectCopy = Object.assign({}, objectInitial);
+
+objectCopy.email = 'baniya@gmail.com'
+
+//simple copy
+//the value of initial object get changed as this process copy the only the refernce of memory location
+
+console.log(objectInitial);
+console.log(objectCopy);
+
+//deep copy of the object is used for the elimination of redundancy in shallow copy
+//note it doesnot work in the case of function and date
+//to prevent this we can use third party library i.e lodash library
+
+const _= require('lodash');
+
+let studentObject = {
+    stname: 'Student',
+    stGrade: 14,
+    getdetails: function(){
+        console.log(`${this.stname} reads in grade ${this.stGrade}`);
+    }
+}
+
+// let studentObjectCopy = JSON.parse(JSON.stringify(studentObject));
+let studentObjectCopy = _.cloneDeep(studentObject);
+
+console.log('Real Object: ', studentObject);
+console.log('Duplicate Object: ', studentObjectCopy);
+
+//summary of todays practice
+// 1) Function GeneratorFunction
+
+// 2)Object deep and shallow copy
 
   
 
