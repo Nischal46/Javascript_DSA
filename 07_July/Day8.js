6cc5d32edd5b68b544616d71f684f0815c7d6f02
Nischal <baniyanisal12@gmail.com>
2024-07-08 21:41:31 +0545

    array and array related important method as filter reduce

---
+++ 07_July/Day8.js
@@ -0,0 +1,56 @@
+// reduce method
+
+let item = [
+    {
+        id: 12,
+        itemName: 'headset',
+        itemPrice: 2500,
+        itemBrand: 'fantech'
+    },
+
+    {
+        id: 14,
+        itemName: 'mouse',
+        itemPrice: 1500,
+        itemBrand: 'fantech'
+    },
+
+    {
+        id: 32,
+        itemName: 'keyboard',
+        itemPrice: 5200,
+        itemBrand: 'eyoso'
+    },
+
+    {
+        id: 43,
+        itemName: 'mousepad',
+        itemPrice: 250,
+        itemBrand: 'fantech'
+    }
+]
+
+let filteritem = item.reduce((acc, cur) => {
+    if (cur.itemPrice > 2000) {
+        acc.push(cur);
+        // console.log(cur)
+    }
+    return acc;
+}, [])
+
+console.log(filteritem);
+
+//filter array related the brand
+
+let filterbrand = item.filter((cl) => cl.itemBrand === 'fantech');
+console.log('The brand filter is ', filterbrand);
+
+//calculte the sum of whole product
+
+let totalsum = item.reduce((acc, cur) => {
+    return acc + cur.itemPrice;
+}, 0)
+
+console.log('The total sum of the item is', totalsum);
+
+
