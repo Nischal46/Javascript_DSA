5ae523e9d2cf6dbd050877adb71dcf2d218a32a7
Nischal <baniyanisal12@gmail.com>
2024-07-09 22:05:09 +0545

    reduce method and flat map concept

---
+++ 07_July/Day9.js
@@ -0,0 +1,24 @@
+// reduce method
+
+let arr = [12,34,45,65,77,87,98];
+
+let divisiblebytwo = arr.reduce((acc, cur) => {
+    if(cur % 2 == 0) {
+        console.log(`${cur} is divisible by two`);
+        acc.push(cur)
+    }
+    return acc
+}, [])
+
+console.log(divisiblebytwo);
+
+let numarr = [12,23,43,54,[65,343,233, [55,67]]];
+
+//works for only one nested
+let flatmapping = numarr.flatMap(el => el);
+
+console.log(flatmapping);
+
+//works for multiple nested
+let flatdeepnested = numarr.flat(Infinity);
+console.log(flatdeepnested);
