057da55f7dfffafaed4624b17055e35f3249b248
Nischal <baniyanisal12@gmail.com>
2024-08-16 22:14:44 +0545

    counting letter of string and remove white space and character and then displaying in objects

--- 08_Aug/Day16.js
+++ 08_Aug/Day16.js
@@ -12,3 +12,25 @@ function Questionno1() {
     console.log('hashed password: ', hashedpassword);
 
 }
+
+//----------------------------------Question no 2---------------------------------------
+
+function Questionno2() {
+    let lettercounting = 'aasf!!ds dsfs@!f fd..sf 12fdfs';
+
+    lettercounting = lettercounting.split('').filter((acc) => acc !== '@' && acc !== ' ' && acc !== '.' && acc !== '!').reduce((acc, cv) => {
+        if (!acc[cv]) {
+            acc[cv] = 1
+        }
+        else {
+            acc[cv] += 1
+        }
+
+        return acc;
+    }, {})
+
+    return lettercounting;
+}
+
+console.log(Questionno2());
+
