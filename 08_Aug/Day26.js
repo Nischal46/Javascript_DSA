461d4bf55a43b3a4468f37e756474fe6afb832a2
Nischal <baniyanisal12@gmail.com>
2024-08-29 22:57:47 +0545

    string count method using reduce array method in js

--- 08_Aug/Day26.js
+++ 08_Aug/Day26.js
@@ -1,14 +1,31 @@
 let newArray = [
-    {id: 123, product: 'mouse', price: 1500},
-    {id: 234, product: 'keyboard', price: 5000},
-    {id: 231, product: 'mousepad', price: 500},
-    {id: 345, product: 'cooler', price: 2500},
-    {id: 787, product: 'laptop', price: 78000}
+    { id: 123, product: 'mouse', price: 1500 },
+    { id: 234, product: 'keyboard', price: 5000 },
+    { id: 231, product: 'mousepad', price: 500 },
+    { id: 345, product: 'cooler', price: 2500 },
+    { id: 787, product: 'laptop', price: 78000 }
 ]
 
 newArray = newArray.reduce((acc, cur) => {
-    if(cur.price > 1000) acc.push(cur)
-        return acc
+    if (cur.price > 1000) acc.push(cur)
+    return acc
 }, [])
 
 console.log(newArray);
+
+
+//counting the letter in string
+
+let stringcount = 'Practice makes man perfect in his work';
+
+stringcount = stringcount.toLocaleLowerCase().split("").reduce((acc, cur) => {
+    if(!acc[cur]) {
+        if (acc[cur] !== "") {
+            acc[cur] = 1;
+        }
+    }
+    else acc[cur]++;
+    return acc;
+}, {})
+
+console.log(stringcount);
