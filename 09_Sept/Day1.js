8bd1e06d7a5c062330e3a4ddb12af53b5c34cf1e
Nischal <baniyanisal12@gmail.com>
2024-09-01 23:33:58 +0545

    deep copy and shallow copy

--- 09_Sept/Day1.js
+++ 09_Sept/Day1.js
@@ -14,3 +14,24 @@ let randomString = 'i want to code every day';
 randomString = randomString.split(" ").join("-");
 
 console.log('Replaced string: ', randomString);
+
+//deep copy and shallow copy
+
+const object1 = {
+    name: 'random',
+    features: ['chair', 'table']
+}
+
+const object2 = object1;
+
+object2.features[1] = "Desk";
+
+// console.log('copying with normal memory reference: ', object1); //this is just referencing memory so this will affect real object
+
+const object3 = JSON.parse(JSON.stringify(object1)); //this is deep cloning
+
+object3.features[1] = "Couch";
+
+console.log(object1, object3);
+
+
