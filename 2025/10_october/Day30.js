01f387b23aeee69142909f30a2f6329b1cd75533
Nischal Baniya <baniyanisal12@gmail.com>
2025-10-30 21:14:52 +0545

    revise double linked list with insertion of node at first and last of head and tail

---
+++ 2025/10_october/Day30.js
@@ -0,0 +1,48 @@
+//NOTE: Implementation of Double Linked list as it can traverse for both forward and backward node
+
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.previous = null;
+    this.next = null;
+  }
+}
+
+class DoubleLinkList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  appendAtFirst(val) {
+    let newNode = new Node(val);
+    if (this.head === null) {
+      this.head = this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head.previous = newNode;
+      this.head = newNode;
+    }
+  }
+
+  appendAtLast(val) {
+    let newNode = new Node(val);
+    if (this.tail === null) {
+      this.tail = this.head = newNode;
+    } else {
+      newNode.previous = this.tail;
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+  }
+
+  appendAtSpecificPosition() {}
+}
+
+const dll = new DoubleLinkList();
+dll.appendAtFirst(4);
+dll.appendAtFirst(3);
+dll.appendAtLast(7);
+dll.appendAtFirst(2);
+
+console.log(dll);
