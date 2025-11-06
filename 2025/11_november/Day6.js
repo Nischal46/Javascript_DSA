b11f457cb7858f36fa2029fb1874d10f14110eac
Nischal Baniya <baniyanisal12@gmail.com>
2025-11-06 09:23:53 +0545

    logic implementation of linked list of append delete at nth index and traverse all the element

---
+++ 2025/11_november/Day6.js
@@ -0,0 +1,67 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  append(data) {
+    const newNode = new Node(data);
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      let currentNode = this.head;
+      while (currentNode.next !== null) {
+        currentNode = currentNode.next;
+      }
+      currentNode.next = newNode;
+    }
+  }
+
+  remove(position) {
+    let temp = this.head;
+
+    if (position === 1) {
+      this.head = temp.next;
+      return this.head;
+    }
+
+    let prev = null;
+    for (let i = 1; i < position; i++) {
+      prev = temp;
+      temp = temp.next;
+    }
+
+    prev.next = temp.next;
+    return this.head;
+  }
+
+  print() {
+    let curr = this.head;
+    while (curr !== null) {
+      process.stdout.write(`[${curr.data}]` + "->");
+      curr = curr.next;
+    }
+    console.log("null");
+  }
+}
+
+let ll = new LinkedList();
+ll.append(22);
+ll.append(34);
+ll.append(46);
+
+console.dir(ll, { depth: Infinity });
+ll.print();
+
+ll.remove(3);
+ll.append(56);
+ll.append(67);
+
+console.dir(ll, { depth: Infinity });
+ll.print();
