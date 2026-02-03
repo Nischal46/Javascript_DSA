22b4b075d8bb6ae9e79d1eb535c6c67287eeaca7
Nischal Baniya <baniyanisal12@gmail.com>
2026-02-03 23:44:36 +0545

    revise linked list in different

---
+++ 2026/02_February/Day3.js
@@ -0,0 +1,49 @@
+//Linked List
+class LinkedListNode {
+  constructor(value, next = null) {
+    this.value = value;
+    this.next = next;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+
+    //head and tail both are nodes
+  }
+
+  prepend(value) {
+    const newNode = new LinkedListNode(value, this.head);
+    this.head = newNode;
+
+    if (!this.tail) {
+      this.tail = newNode;
+    }
+
+    return this;
+  }
+
+  append(value) {
+    const newNode = new LinkedListNode(value);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+
+      return this;
+    }
+
+    this.tail.next = newNode;
+    this.tail = newNode;
+
+    return this;
+  }
+}
+
+const ll = new LinkedList();
+ll.append(33);
+ll.append(45);
+
+console.log("logging linked list: ", ll);
