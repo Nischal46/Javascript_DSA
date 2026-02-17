bd234f3151d5c20aa7dac2e6f9be8469e9584688
Nischal Baniya <baniyanisal12@gmail.com>
2026-02-17 23:14:48 +0545

    revision

---
+++ 2026/02_February/Day17.js
@@ -0,0 +1,37 @@
+let array = [23, 54, 66, 76, 87, 89, 97];
+
+let removingarray = array.splice(0, 2);
+
+console.log(removingarray);
+
+let num1 = [12, 23, 34, 45, 56, 34, 34, 76, 45];
+
+function removeDuplicate(no){
+    let removeDuplicateNumber = []
+    let highestnum = 0;
+    let lowestnum = no[0];
+    
+    for(const item of no){
+       
+        if(!removeDuplicateNumber.includes(item)){
+            removeDuplicateNumber.push(item)
+        }
+        if(item > highestnum) highestnum = item;
+
+        if(item < lowestnum) lowestnum = item;
+    
+    }
+
+    
+    console.log('The largest number in the array is ', highestnum);
+    console.log('The smallest number in the array is ', lowestnum);
+
+    return removeDuplicateNumber;
+}
+
+console.log(removeDuplicate(num1));
+
+let name = 'nischal baniya';
+
+let slicestring = name.slice(0, 7);
+console.log(slicestring);
